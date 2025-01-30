14d7e27752fd97589bd3897aa0eec35e5e80d7bf
Valerij Zakharenko <konvpalto87@gmail.com>
2025-01-30 07:12:22 +0100

    remove redundant comment

--- MusicMate.AppHost/Program.cs
+++ MusicMate.AppHost/Program.cs
@@ -3,9 +3,4 @@ var builder = DistributedApplication.CreateBuilder(args);
 var apiService = builder
     .AddProject<Projects.MusicMate_ApiService>("apiservice");
 
-// builder.AddProject<Projects.MusicMate_Web>("webfrontend")
-//     .WithExternalHttpEndpoints()
-//     .WithReference(apiService)
-//     .WaitFor(apiService);
-
 builder.Build().Run();
