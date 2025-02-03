290a9bec18317a9dc5d1ca3f8226855743e9a051
Valerij Zakharenko <konvpalto87@gmail.com>
2025-02-03 20:12:38 +0100

    Expose backend api

--- MusicMate.AppHost/Program.cs
+++ MusicMate.AppHost/Program.cs
@@ -1,6 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder
-    .AddProject<Projects.MusicMate_ApiService>("apiservice");
+    .AddProject<Projects.MusicMate_ApiService>("apiservice")
+    .WithExternalHttpEndpoints();
 
 builder.Build().Run();
